3e7b300b539204ab2a34a9136ebdbfe3731b1fc8
Ryan Barnes <barnesry@juniper.net>
2016-05-17 11:14:03 -0700

    tested and working on influx0.13

--- netconf-poll.py
+++ netconf-poll.py
@@ -7,7 +7,7 @@ password = "junos123"
 from jnpr.junos import Device
 from jnpr.junos.op.ethport import EthPortTable
 from time import sleep
-from influxdb.influxdb08 import client
+from influxdb import InfluxDBClient
 
 device = Device(host=device_ip, port=22, user=user_name, passwd=password)
 device.open()
@@ -15,17 +15,45 @@ switch_name = device.facts['fqdn']
 print 'Connected to', switch_name, '(', device.facts['model'], 'running', device.facts['version'], ')'
 ports_table = EthPortTable(device)
 
-db = client.InfluxDBClient('localhost', 8086, 'root', 'root', 'network')
+db = InfluxDBClient('localhost', 8086, 'root', 'root', 'network')
 print 'Connected to InfluxDB'
 
 print 'Collecting metrics...'
 columns = ['rx_packets', 'rx_bytes', 'tx_packets', 'tx_bytes']
+
+json_body = [
+    {
+        "measurement": "cpu_load_short",
+        "tags": {
+            "host": "server01",
+            "region": "us-west"
+        },
+        "time": "2009-11-10T23:00:00Z",
+        "fields": {
+            "value": 0.64
+        }
+    }
+]
+
 while True:
     ports = ports_table.get()
     for port in ports:
-        point = {'name': switch_name + '.' + port['name'],
-                 'columns': columns,
-                 'points': [
-                     [int(port['rx_packets']), int(port['rx_bytes']), int(port['tx_packets']), int(port['tx_bytes'])]]}
-        db.write_points([point])
-    sleep(1)
+        if port.name.startswith("ge"):
+            point = [
+                {
+                    'measurement': 'port_counters',
+                    'tags': {
+                        'device':   switch_name,
+                        'port':     port['name']
+                    },
+                    'fields': {
+                            'rx_packets':   int(port['rx_packets']),
+                            'rx_bytes':     int(port['rx_bytes']),
+                            'tx_packets':   int(port['tx_packets']),
+                            'tx_bytes':     int(port['tx_bytes'])
+                    }
+                }
+            ]
+            print point
+            db.write_points(point)
+        sleep(1)
